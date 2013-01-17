56dac693af88d5dad655bea232fefea283a98806
Stanton <stantonwjones@gmail.com>
2013-01-17 15:01:50 -0500

    add the basic outline of the game

---
+++ frontend/scripts/lib/game.js
@@ -0,0 +1,9 @@
+/**
+ *  Defines the game object
+ *
+ *  A game has two players and keeps track of
+ *  whose turn it is and evaluates the win
+ *  conditions at the end of each turn.
+ */
+function Game() {
+}
