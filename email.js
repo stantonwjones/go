4a1d45e71f6b8c131e4a8cf82cdee17838aaa492
Stanton <stantonwjones@gmail.com>
2013-01-31 18:58:05 -0500

    email test works

--- email.js
+++ email.js
@@ -1,14 +1,19 @@
 email = require('emailjs');
+var password = require('./password.js').password;
+console.log(password);
 var server = email.server.connect({
     user: 'admin@stantonwjones.com',
     host: 'smtpout.secureserver.net',
     port: 465,
     ssl: 'true',
-    password: require('./password.js')
+    password: password
 });
 
 server.send({
     text: 'test',
     to: 'stantonwjones@gmail.com',
     from: 'go@stantonwjones.com'
+}, function (err, message) {
+    console.log( err || message );
+    process.exit();
 });
